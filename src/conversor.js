f99bea840017fdca9cd9d37d2221c9015eda5aa7
Darwel Quintero <darwel34@gmail.com>
2017-06-29 13:10:15 -0430

    se elimino espacios

--- src/conversor.js
+++ src/conversor.js
@@ -1,9 +1,8 @@
-exports.Conversor  = class{
+exports.Conversor = class{
   constructor(){
     this.baseLibras= 2.20462;
     this.baseKilogramos= 0.453592;
   }
-
 
   librasToKilogramos(_libras){
     return _libras * this.baseKilogramos
